84e425aa649bd29d8c59869aa5eb1ca9301cea7e
tomerkassorla01 <tomer.kassorla@post.runi.ac.il>
2024-11-16 15:58:40 +0200

    Done all

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,21 @@
-// Prints a crowd cheering output.
+
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            String cheeringInput = args[0].toUpperCase();
+            int counter = Integer.parseInt(args[1]);
+            String needAnChars = "AEFHILMNORSX";
+            for (int i = 0; i < cheeringInput.length(); i++) {
+                char c1 = cheeringInput.charAt(i);
+                if (needAnChars.indexOf(c1) != -1) {
+                    System.out.println("Give me an " + c1 + ": " + c1 + "!");
+                } 
+                else {
+                    System.out.println("Give me a " + c1 + ": " + c1 + "!");
+                }
+            }
+            System.out.println("What does that spell?");
+            for (int i = 0; i < counter; i++) {
+                System.out.println(cheeringInput + "!!!");
+            }
         }
 }
